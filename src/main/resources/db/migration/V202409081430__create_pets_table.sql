2da90081f0128065ce7285d3c169224427f92d20
Matan Sabag <matan.sabag@hibob.io>
2024-09-08 16:01:02 +0300

    finish the Exercise

--- src/main/resources/db/migration/V202409081430__create_pets_table.sql
+++ src/main/resources/db/migration/V202409081430__create_pets_table.sql
@@ -1,9 +1,11 @@
 create table pets
 (
-    id UUID primary key default gen_random_uuid() ,
+    id serial primary key,
     name varchar(100) not null ,
     type varchar(100) not null ,
     company_id int,
     date_of_arrival date not null
 );
+/
 CREATE INDEX idx_name_company_id ON pets(company_id);
+c
--- src/main/resources/db/migration/V202409081445__create_owner_table.sql
+++ src/main/resources/db/migration/V202409081445__create_owner_table.sql
@@ -1,8 +1,21 @@
 create table owner
 (
-    owner_id UUID primary key default gen_random_uuid(),
+    owner_id serial primary key,
     name varchar(100) not null,
     company_id int,
-    employee_id int,
-)
+    employee_id int
+);
 CREATE INDEX idx_name_owner_id_employee_id ON owner(owner_id,employee_id);
+
+/*INSERT INTO pets ( name, type, company_id, date_of_arrival) VALUES
+('Buddy', 'Dog', 101, '2023-01-15'),
+('Mittens', 'Cat', 102, '2022-06-20'),
+('Charlie', 'Dog', 103, '2021-08-05'),
+('Whiskers', 'Cat', 104, '2023-03-10'),
+('Goldie', 'Fish', 105, '2022-09-25');*/
+
+/*SELECT * FROM pets WHERE type = 'Dog';*/
+
+/*DELETE FROM pets WHERE id = 3;*/
+
+/*SELECT * FROM pets WHERE date_of_arrival < (CURRENT_DATE-INTERVAL '1 YEAR')*/
