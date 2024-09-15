39d79d09a15bda6fba1e6dea8cc72367f2fb0cd5
Matan Sabag <matan.sabag@hibob.io>
2024-09-15 14:32:03 +0300

    remove all migration code

--- src/main/resources/db/migration/V2024090111400__create_owner_id_to_pets_table.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE pets
-    ADD owner_id BIGINT;
-
-CREATE UNIQUE INDEX unique_company_owner ON pets(company_id, owner_id);
--- src/main/resources/db/migration/V202409081430__create_pets_table.sql
+++
@@ -1,9 +0,0 @@
-create table pets
-(
-    id BIGSERIAL primary key,
-    name varchar(100) not null ,
-    type varchar(100) not null ,
-    company_id BIGINT,
-    date_of_arrival date DEFAULT CURRENT_DATE
-);
-
