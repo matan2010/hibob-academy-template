1277eee46d2d40d15884c2998b1a4eb0066e043b
Matan Sabag <matan.sabag@hibob.io>
2024-09-16 12:58:41 +0300

    make OwnerService and PetService

--- src/main/kotlin/com/hibob/academy/service/PetService.kt
+++ src/main/kotlin/com/hibob/academy/service/PetService.kt
@@ -6,9 +6,42 @@ import com.hibob.academy.dao.PetType
 
 class PetService(private val petDao : PetDao){
 
-    fun getPetsByType(companyId:Long,type: PetType): List<PetData> = petDao.getPetsByType(companyId,type)
+    fun getPetsByType(companyId:Long,type: PetType): List<PetData> {
+        if(companyId < 0){
+            throw IllegalArgumentException("Invalid companyId")
+        }
+        if(type.equals(null)){
+            throw IllegalArgumentException("Invalid type")
+        }
+        return petDao.getPetsByType(companyId,type)
+    }
 
-    fun insertPet(name: String, type: PetType,companyId: Long ,ownerId:Long?) = petDao.insertPet(name, type,companyId,ownerId)
+    fun insertPet(name: String, type: PetType,companyId: Long ,ownerId:Long?) {
+        if(name==""){
+            throw IllegalArgumentException("Name cannot be empty")
+        }
+        if(type.equals(null)){
+            throw IllegalArgumentException("Type cannot be empty")
+        }
+        if(companyId < 0){
+            throw IllegalArgumentException("Invalid companyId")
+        }
+        if(ownerId == null || ownerId < 0){
+            throw IllegalArgumentException("Invalid ownerId")
+        }
+        return petDao.insertPet(name, type,companyId,ownerId)
+    }
 
-    fun adoptPet(petId: Long, ownerId: Long,companyId :Long) = petDao.adoptPet(petId,ownerId,companyId)
+    fun adoptPet(petId: Long, ownerId: Long,companyId :Long) {
+        if(petId < 0){
+            throw IllegalArgumentException("Invalid petId")
+        }
+        if(ownerId < 0){
+            throw IllegalArgumentException("Invalid ownerId")
+        }
+        if(companyId < 0){
+            throw IllegalArgumentException("Invalid companyId")
+        }
+        return petDao.adoptPet(petId,ownerId,companyId)
+    }
 }
