338b21e932aef22282ceaff2ed43eb0161a35966
Matan Sabag <matan.sabag@hibob.io>
2024-09-16 15:03:08 +0300

    make test

--- src/test/kotlin/com/hibob/academy/service/OwnerServiceTest.kt
+++ src/test/kotlin/com/hibob/academy/service/OwnerServiceTest.kt
@@ -1,5 +1,17 @@
 package com.hibob.academy.service
 
-import org.junit.jupiter.api.Assertions.*
+import com.hibob.academy.dao.OwnerDao
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
 
-class OwnerServiceTest
+class OwnerServiceTest{
+    private val ownerDaoMock = mock<OwnerDao>{}
+    private val ownerService = OwnerService(ownerDaoMock)
+
+    @Test
+    fun `getAllOwner should throw exception if companyId `() {
+
+    }
+}
