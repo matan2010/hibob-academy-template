769890e6c2bd4ddffb559f7222a7720fdcec0790
Matan Sabag <matan.sabag@hibob.io>
2024-09-16 12:58:41 +0300

    make OwnerService and PetService

--- src/main/kotlin/com/hibob/academy/service/OwnerService.kt
+++ src/main/kotlin/com/hibob/academy/service/OwnerService.kt
@@ -5,9 +5,32 @@ import com.hibob.academy.dao.OwnerData
 
 class OwnerService(private val ownerDao : OwnerDao){
 
-    fun getAllOwner(companyId:Long): List<OwnerData> = ownerDao.getAllOwner(companyId)
+    fun getAllOwner(companyId:Long): List<OwnerData> {
+        if(companyId < 0){
+            throw IllegalArgumentException("Company id must be greater than 0")
+        }
+        return ownerDao.getAllOwner(companyId)
+    }
 
-    fun createNewOwner(name:String,employeeId:Long,companyId: Long) = ownerDao.createNewOwner(name, employeeId, companyId)
+    fun createNewOwner(name:String,employeeId:Long,companyId: Long) {
+        if(name == ""){
+            throw IllegalArgumentException("Name must not be empty")
+        }
+        if(employeeId < 0){
+            throw IllegalArgumentException("Employee id must be greater than 0")
+        }
+        if(companyId < 0){
+            throw IllegalArgumentException("Company id must be greater than 0")
+        }
+        return ownerDao.createNewOwner(name, employeeId, companyId)
+    }
 
-    fun getOwnerByPetId(petId: Long, companyId: Long): OwnerData? = ownerDao.getOwnerByPetId(petId, companyId)
+    fun getOwnerByPetId(petId: Long, companyId: Long): OwnerData? {
+        if(petId < 0){
+            throw IllegalArgumentException("Pet id must be greater than 0")
+        }
+        if(companyId < 0){
+            throw IllegalArgumentException("Company id must be greater than 0")
+        }
+        return ownerDao.getOwnerByPetId(petId, companyId)}
 }
