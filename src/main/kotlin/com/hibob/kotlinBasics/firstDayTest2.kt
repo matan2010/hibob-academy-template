498a72b7113dd386ad1fa00520cb1745bc3bdf2c
Matan Sabag <matan.sabag@hibob.io>
2024-09-18 23:27:28 +0300

    fix isValidIdentifier

--- src/main/kotlin/com/hibob/kotlinBasics/firstDayTest2.kt
+++ src/main/kotlin/com/hibob/kotlinBasics/firstDayTest2.kt
@@ -1,3 +1,5 @@
+import com.hibob.kotlinBasics.isValidIdentifier
+
 fun main(args: Array<String>) {
     println(isValidIdentifier("name"))   // true
     println(isValidIdentifier("_name"))  // true
@@ -8,19 +10,3 @@ fun main(args: Array<String>) {
 
 }
 
-class StringValidator {
-
-    fun isValidIdentifier(s: String): Boolean {
-        TODO()
-    }
-
-    fun main(args: Array<String>) {
-        println(isValidIdentifier("name"))   // true
-        println(isValidIdentifier("_name"))  // true
-        println(isValidIdentifier("_12"))    // true
-        println(isValidIdentifier(""))       // false
-        println(isValidIdentifier("012"))    // false
-        println(isValidIdentifier("no$"))    // false
-    }
-
-}
