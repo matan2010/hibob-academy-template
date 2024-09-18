a79e5939ecaba4a855abfb9dbb556a7de855d73c
Matan Sabag <matan.sabag@hibob.io>
2024-09-18 17:21:00 +0300

    make function countPetsByType on PetResource

--- src/main/kotlin/com/hibob/academy/resource/PetResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/PetResource.kt
@@ -1,4 +1,31 @@
 package com.hibob.academy.resource
 
-class PetResource {
+
+
+
+import com.hibob.academy.service.PetService
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.PUT
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/matan/sabag/pets/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class PetResource (private val petService: PetService) {
+
+
+
+    @GET
+    @Path("countPetsByType/{companyId}")
+    fun countPetsByType( @PathParam("companyId") companyId: Long): Response {
+        return Response.ok(petService.countPetsByType(companyId)).build()
+    }
 }
