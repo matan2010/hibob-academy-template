555ba98544802fd72e53181269182652b0180d52
Matan Sabag <matan.sabag@hibob.io>
2024-09-18 23:21:40 +0300

    fix all problem

--- src/main/kotlin/com/hibob/kotlinBasics/Main.kt
+++ src/main/kotlin/com/hibob/kotlinBasics/Main.kt
@@ -1,36 +1,32 @@
-package com.hibob.kotlinBasics//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.hibob.kotlinBasics
+
 fun main() {
-    //“hello world” program
     fun helloWorld() {
         println("hello world")
     }
-    //function that prints if the given number is even or odd
-    fun evenOrOdd(a: Int){
+
+    fun evenOrOdd(a: Int) {
         if (a % 2 == 0) println("even") else println("odd")
     }
-    //function that checks whether two integers are equal or not
-    fun isEqual(a: Int ,b:Int):Boolean {
-        if (a == b)  return true  else return false
-    }
-    //function as an expression (one line) that prints the max value of two given integers
-    fun max(a:Int ,b:Int):Int = if (a > b) a else b
 
-    fun multiplication(a: Int = 1, b: Int = 1):Int = a * b
-    //many possibilities to call fun multiplication
-    multiplication(a=1, b=2)
-    multiplication(b=2)
-    multiplication(a=3)
+    fun isEqual(a: Int, b: Int): Boolean = a == b
+
+    fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+    fun multiplication(a: Int = 1, b: Int = 1): Int = a * b
+    multiplication(a = 1, b = 2)
+    multiplication(b = 2)
+    multiplication(a = 3)
     multiplication()
-    multiplication(b=2,a=4)
+    multiplication(b = 2, a = 4)
 }
 
 fun isValidIdentifier(s: String): Boolean {
-    if(s.length < 1) return false
-    val a = s.get(0)
-    if('0'<= a && '9' >= a) return false
-    for (i in s){
-       if(!((i=='_'||'A'<= i && 'Z'>= i) || ('a'<= i && 'z'>= i )||('0'<= i && '9'>= i) ) ) return false
+    if (s.isEmpty()) return false
+    if (s[0].isDigit()) return false
+
+    for (char in s) {
+        if (!char.isLetterOrDigit() && char != '_') return false
     }
     return true
 }
