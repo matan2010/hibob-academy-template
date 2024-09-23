f4f599f7c51f22be5e91d9e50a8426bc23596c62
Matan Sabag <matan.sabag@hibob.io>
2024-09-23 18:54:22 +0300

    make tests to viewAllFeedback and insertFeedback

--- src/main/kotlin/com/hibob/academy/employeeFeedback/dao/Tables.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/Tables.kt
@@ -9,7 +9,7 @@ class FeedbackTable(tableName: String = "feedback") : JooqTable(tableName) {
     val employeeId = createBigIntField("employee_id")
     val companyId = createBigIntField("company_id")
     val date = createLocalDateField("date")
-    val department = createVarcharField("description")
+    val department = createVarcharField("department")
     val status = createVarcharField("status")
 
     companion object {
--- src/test/kotlin/com/hibob/academy/employeeFeedback/dao/FeedbackDaoTest.kt
+++ src/test/kotlin/com/hibob/academy/employeeFeedback/dao/FeedbackDaoTest.kt
@@ -3,10 +3,10 @@ package com.hibob.academy.employeeFeedback.dao
 import com.hibob.academy.utils.BobDbTest
 import org.jooq.DSLContext
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import java.sql.Date
 import java.time.LocalDate
 
 @BobDbTest
@@ -22,11 +22,28 @@ class FeedbackDaoTest @Autowired constructor(private val sql: DSLContext) {
     }
 
     @Test
-    fun `insertFeedback should be successful`() {
-        val feedback1 = Feedback("Hi", 5L, companyId, LocalDate.now(), "Dev", "UNREVIEWED")
-        val feedback2 = Feedback("By", 6L, companyId, LocalDate.now(), "Hr", "UNREVIEWED")
-        dao.insertFeedback(feedback1)
-        dao.insertFeedback(feedback2)
+    fun `viewAllFeedback should be successful`() {
+        val listFeedback = listOf(
+            Feedback("Hi", 5L, companyId, LocalDate.now(), "Dev", "UNREVIEWED"),
+            Feedback("By", null, companyId, LocalDate.now(), "Hr", "UNREVIEWED")
+        )
+        dao.insertFeedback(listFeedback[0])
+        dao.insertFeedback(listFeedback[1])
+        val allFeedback = dao.viewAllFeedback(companyId)
+        assertEquals(listFeedback.size, allFeedback.size)
+        for (i in listFeedback.indices) {
+            assertEquals(listFeedback[i].feedback, allFeedback[i].feedback)
+            assertEquals(listFeedback[i].employeeId, allFeedback[i].employeeId)
+            assertEquals(listFeedback[i].companyId, allFeedback[i].companyId)
+            assertEquals(listFeedback[i].data, allFeedback[i].data)
+            assertEquals(listFeedback[i].status, allFeedback[i].status)
+        }
     }
 
+    @Test
+    fun `insertFeedback should be successful`() {
+        val feedback = Feedback("Hi", 5L, companyId, LocalDate.now(), "Dev", "UNREVIEWED")
+        val isInsert = dao.insertFeedback(feedback)
+        assert(isInsert)
+    }
 }
