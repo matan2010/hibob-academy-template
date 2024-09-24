224af8e533e122b758ec66f6f84dc08b697b0bab
Matan Sabag <matan.sabag@hibob.io>
2024-09-24 18:45:57 +0300

    add Path

--- src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
@@ -1,5 +1,16 @@
 package com.hibob.academy.employeeFeedback.resource
 
-class FeedbackResource {
+import com.hibob.academy.employeeFeedback.service.FeedbackService
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/employee")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class FeedbackResource (private val feedbackService: FeedbackService) {
 }
 
