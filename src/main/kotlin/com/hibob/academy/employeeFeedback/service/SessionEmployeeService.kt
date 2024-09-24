1dd68e2c870bac2196fb930b07ddff1548587b02
Matan Sabag <matan.sabag@hibob.io>
2024-09-24 17:21:22 +0300

    rename SessionEmployeeService

--- src/main/kotlin/com/hibob/academy/employeeFeedback/service/SessionEmployeeService.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/service/SessionEmployeeService.kt
@@ -4,14 +4,13 @@ import com.hibob.academy.employeeFeedback.dao.Employee
 import com.hibob.academy.employeeFeedback.dao.EmployeeDao
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
-import jakarta.ws.rs.BadRequestException
 import jakarta.ws.rs.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-class SessionService @Autowired constructor(private val employeeDao: EmployeeDao) {
+class SessionEmployeeService @Autowired constructor(private val employeeDao: EmployeeDao) {
     companion object {
         const val SECRET_KEY =
             "secretsdfghjkjhghjhghjhjkjhghjkjhgfghjhg21243e5wredwedywe5te4343tewqawsertyusdfghjkerftgyhujdfgxdewefcvhj"
