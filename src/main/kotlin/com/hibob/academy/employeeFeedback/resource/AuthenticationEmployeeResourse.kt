c4e3ad59e15d5f0024a1d262ebb6c885f47961fc
Matan Sabag <matan.sabag@hibob.io>
2024-09-24 17:32:53 +0300

    rename SessionEmployeeService

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/AuthenticationEmployeeResourse.kt
@@ -0,0 +1,29 @@
+package com.hibob.academy.employeeFeedback.resource
+
+import com.hibob.academy.employeeFeedback.dao.Employee
+import com.hibob.academy.employeeFeedback.service.SessionEmployeeService
+import com.hibob.academy.resource.User
+import com.hibob.academy.service.SessionService
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.NewCookie
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/jwt/employee")
+class AuthenticationEmployeeResourse(private val service: SessionEmployeeService) {
+
+    @Path("/login")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun registerEmployee(employee: Employee): Response {
+        val tokenJwt = service.createJWTToken(employee)
+        val cookie = NewCookie.Builder("matan_name").value(tokenJwt).build()//Creating new cookie
+        return Response.ok().cookie(cookie).build()
+    }
+}
