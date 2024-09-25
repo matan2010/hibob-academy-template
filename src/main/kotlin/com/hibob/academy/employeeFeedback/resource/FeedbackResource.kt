f70ffcf6e11c0ce9680abffb8cc32aed19a482b5
Matan Sabag <matan.sabag@hibob.io>
2024-09-25 09:31:36 +0300

    found who to get the value from cookies

--- src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
@@ -2,7 +2,12 @@ package com.hibob.academy.employeeFeedback.resource
 
 import com.hibob.academy.employeeFeedback.dao.Feedback
 import com.hibob.academy.employeeFeedback.service.FeedbackService
+import com.hibob.academy.employeeFeedback.service.SessionEmployeeService
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.ws.rs.*
+import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
@@ -11,14 +16,35 @@ import org.springframework.stereotype.Controller
 @Path("/api/employee")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class FeedbackResource(private val feedbackService: FeedbackService) {
+class FeedbackResource(private val feedbackService: FeedbackService,
+    @Context private val request: HttpServletRequest) {
 
     @Path("/feedback")
     @GET
-    fun viewAllFeedback(@CookieParam("companyId") companyId: Long,
-                        @CookieParam("role") role: String
-    ): Response {
-        return Response.ok(feedbackService.viewAllFeedback(companyId)).build()
+    fun viewAllFeedback(): Response {
+        val cookies = request.cookies
+        val jwtCookie = cookies.find { it.name == "matan_name" }
+
+        if (jwtCookie == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                .entity("JWT cookie 'matan_name' is missing.")
+                .build()
+        }
+
+        val claims: Claims = try {
+            Jwts.parser()
+                .setSigningKey(SessionEmployeeService.SECRET_KEY) // Ensure you use the correct key
+                .parseClaimsJws(jwtCookie.value)
+                .body
+        } catch (e: Exception) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                .entity("Invalid JWT token.").build()
+        }
+        val companyId =  claims["companyId"]?.let { it as? Number }?.toLong()
+        val role = claims["role"]?.toString()
+        val department = claims["department"]?.toString()
+        //return Response.ok(feedbackService.viewAllFeedback(companyId)).build()
+        return Response.ok().build()
     }
 
     @Path("/feedback")
