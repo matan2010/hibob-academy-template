ef2ae11344cb96ccbed519fdfdb586d8196c386f
Matan Sabag <matan.sabag@hibob.io>
2024-09-26 14:36:15 +0300

    make test

--- src/main/kotlin/com/hibob/academy/employeeFeedback/dao/ResponseDao.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/ResponseDao.kt
@@ -1,11 +1,11 @@
 package com.hibob.academy.employeeFeedback.dao
 
 import org.jooq.DSLContext
+import org.springframework.stereotype.Component
 import java.time.LocalDate
-
+@Component
 class ResponseDao(private val sql: DSLContext) {
     private val responseTable = ResponseTable.instance
-
 
     fun insertResponse(feedbackResponse: FeedbackResponse, employeeId: Long): Boolean {
         return sql.insertInto(responseTable)
--- src/test/kotlin/com/hibob/academy/employeeFeedback/service/ResponseServiceTest.kt
+++ src/test/kotlin/com/hibob/academy/employeeFeedback/service/ResponseServiceTest.kt
@@ -1,5 +1,22 @@
 package com.hibob.academy.employeeFeedback.service
 
-import org.junit.jupiter.api.Assertions.*
+import com.hibob.academy.employeeFeedback.dao.FeedbackResponse
+import com.hibob.academy.employeeFeedback.dao.ResponseDao
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
-class ResponseServiceTest
+class ResponseServiceTest(){
+    private val responseDaoMok = mock<ResponseDao> {}
+    private val responseService = ResponseService(responseDaoMok)
+
+
+    @Test
+    fun `insertResponse should be successful`() {
+        val response= FeedbackResponse(1L,"Hi")
+        whenever(responseDaoMok.insertResponse(response,7L)).thenReturn(true)
+        responseService.insertResponse(response,7L)
+        verify(responseDaoMok).insertResponse(response,7L)
+    }
+}
