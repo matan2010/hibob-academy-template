cf2533d5737e40d8ae4469413e65150124416328
Matan Sabag <matan.sabag@hibob.io>
2024-09-26 12:38:15 +0300

    make a ResponseService // need to complete

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/service/ResponseService.kt
@@ -0,0 +1,12 @@
+package com.hibob.academy.employeeFeedback.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ResponseService(){// @Autowired constructor(private val ResponseDao){
+
+//    fun insertResponse(feedbackResponse: FeedbackResponse, employeeId: Long): Boolean {
+//        ResponseDao(feedbackResponse,employeeId)
+//    }
+}
